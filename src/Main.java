8ad58d92b8518510ffcc86b9f76b6656cd7c2ae9
Nikita Fomin <6239651+sunrize@users.noreply.github.com>
2023-03-14 20:24:02 +0500

    Поменял везде типы продаж с int на long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {100,200,300};
+        long[] sales = {100,200,300};
         SalesManager manager = new SalesManager(sales);
 
         System.out.println(manager.max());
